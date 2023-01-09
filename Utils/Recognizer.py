5ffc41339f6fbb375ed7a7aa96196382b5fe3eed
Ahmad-Qerem <alqerema@gmail.com>
2023-01-09 18:42:59 +0200

    edit recognizer

--- Utils/Recognizer.py
+++ Utils/Recognizer.py
@@ -16,12 +16,14 @@ class Recognizer:
         self.mic = sr.Microphone(device_index=1)
         # Remove noise from the background
         with self.mic as source:
-            self.recognizer.adjust_for_ambient_noise(source, duration=1)
+            self.recognizer.adjust_for_ambient_noise(source)
+            
+        self.recognizer.adjust_for_ambient_noise(self.mic)
         print("Recognizer Is Ready To Listen ")
 
     def StartListen(self, callBack):
         self.ThreadListenInBackGround = self.recognizer.listen_in_background(
-            source=self.mic, callback=callBack)
+            source=self.mic, callback=callBack , phrase_time_limit=6)
 
     def StopListen(self):
         self.ThreadListenInBackGround()
