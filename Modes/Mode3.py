074ab8622d7f3399eee0f8c12533d9b598d0a1d8
Ahmad-Qerem <alqerema@gmail.com>
2023-01-17 20:09:32 +0200

    fix mode 3 bug

--- Modes/Mode3.py
+++ Modes/Mode3.py
@@ -1,4 +1,5 @@
 
+from time import sleep
 from Utils.Recognizer import Recognizer
 from ttgLib.TextToGcode import ttg
 
@@ -6,17 +7,19 @@ class Mode3:
     def __init__(self, BluetoothSerial):
         self.BS = BluetoothSerial
         self.recognizer = Recognizer()
+        self.ExitFlag=False
         print("New Mode3 Object Has been created ")
 
     def callBack(self, recognizer, audio):
         print("callBack Mode3")
         try:
             word = recognizer.recognize_google(audio_data=audio, key=None, language='en-US')
-            Data = ttg(word, 1, 0, "return", 6000).toGcode(
+            Data = ttg(word, 5, 0, "return", 6000).toGcode(
                 "M5", "M3S90", "G0", "G1")
             print(Data)
             self.BS.SendList(Data)
             if self.word == "disconnect":
+                self.ExitFlag=True
                 self.recognizer.StopListen()
             else:
                 print(" Something ... Mode 3")
@@ -31,5 +34,7 @@ class Mode3:
 
     def ActiveMode3(self):
         self.recognizer.StartListen()
+        while not self.ExitFlag:
+            sleep(0.5)
 
         
